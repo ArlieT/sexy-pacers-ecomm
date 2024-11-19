ecf5376d1fd6c35084ed6a217223bb172a62eae4
arlieboYk@22 <torresarlie22@gmail.com>
2024-11-20 00:16:25 +0800

    chore:(helper): normalizeEmail

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,3 +7,11 @@ export const generateUserId = () => {
 export const generateVerificationToken = () => {
   return randomBytes(32).toString("hex");
 };
+
+export const normalizeEmail = (email: string) => {
+  const [localPart, domain] = email.split("@");
+  if (domain === "gmail.com") {
+    return localPart.replace(/\./g, "") + "@" + domain;
+  }
+  return email; // Leave non-Gmail emails untouched
+};
